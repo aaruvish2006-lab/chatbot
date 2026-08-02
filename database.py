13cef77aa34b2ca8fe011336b7d3fb8b05f23a2f
aaruvish2006-lab <aaru.vish2006@gmail.com>
2026-08-02 20:57:25 +0530

    Update code change

--- database.py
+++ database.py
@@ -1,2 +1,8 @@
 def connect_database():
-    pass
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",      # Change this if your MySQL password is different
+        database="chatbot"
+    )
+    return db
